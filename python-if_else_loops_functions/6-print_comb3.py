b5bf33df4d2770d9ed805133452ecda68ba39bf5
Zeynab <zsakho58@gmail.com>
2025-01-17 15:01:28 +0100

    print combs

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        if num1 < 8:
+            print("{:02d}{:02d}".format(num1, num2), end=", ")
+        else:
+            print("{:02d}{:02d}".format(num1, num2))
