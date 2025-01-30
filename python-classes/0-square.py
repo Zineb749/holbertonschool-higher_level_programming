8b1b0a40717472a0cf4b4c9943a38021e18e222d
Zeynab <zsakho58@gmail.com>
2025-01-30 10:23:42 +0100

    pycode fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
- """ Empty class square that represents a square"""
-
- class Square:
-
 """ Empty class square that represents a square"""
+
+
+class Square:
+
+    """ Empty class square that represents a square"""
 
     pass
