25902a14e15ca3245d0eee13157db3a39dd29d7e
Zeynab <zsakho58@gmail.com>
2025-02-05 18:35:43 +0100

    pycode fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+
 """Check if obj is exactly an instance of a_class"""
+
+
 def is_same_class(obj, a_class):
     """Check if obj is exactly an instance of a_class"""
-    return type(obj) == a_class 
+    return type(obj) == a_class
