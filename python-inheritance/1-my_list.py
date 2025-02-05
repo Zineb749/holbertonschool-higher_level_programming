b1fa4fc056a6ca90a19b771850d98c96af9db268
Zeynab <zsakho58@gmail.com>
2025-02-05 20:00:41 +0100

    code fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
-
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
+    """A class that inherits from list and prints a sorted version of itself."""
+    
     def print_sorted(self):
-        """prints the sorted list"""
+        """Prints the list in ascending sorted order without modifying the original list."""
         print(sorted(self))
+
+if __name__ == "__main__":
+    MyList = __import__('my_list').MyList
+
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
+
 
