20f58abc9bd457fd87f20cd6345352b2dc2f940b
Zeynab <zsakho58@gmail.com>
2025-02-05 20:05:04 +0100

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
+"""class MyList that inherits from list"""
+
 
 class MyList(list):
-    """A class that inherits from list and prints a sorted version of itself."""
-    
+    """inherits from list"""
     def print_sorted(self):
-        """Prints the list in ascending sorted order without modifying the original list."""
+        """print the sorted list"""
         print(sorted(self))
-
-if __name__ == "__main__":
-    MyList = __import__('my_list').MyList
-
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
-
-
