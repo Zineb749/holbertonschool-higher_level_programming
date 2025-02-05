0b0409f2c52165bc56b25a47787a93ece45fba4f
Zeynab <zsakho58@gmail.com>
2025-02-05 19:36:11 +0100

    exeption

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python3
+"""base geometry class"""
+
+
+class BaseGeometry:
+
+    def area(self):
+
+        """empty base geometry class"""
+
+
+raise Exception
+
+
+print("area() is not implementeed")
