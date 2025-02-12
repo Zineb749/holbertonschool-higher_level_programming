a767c0a94e2d0877de65c87456e6e397de8cb842
Zeynab <zsakho58@gmail.com>
2025-02-12 14:02:26 +0100

    code fix

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """saves object to json file"""
-    with open(my_obj.json, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, filename)
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
