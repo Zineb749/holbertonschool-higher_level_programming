f2c40f58306749485e7cbe18b1b6d2381020a648
Zeynab <zsakho58@gmail.com>
2025-02-13 15:08:27 +0100

    fix code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
+"""
+Script that add all argument to a Python list and save them to a JSON file.
+
+"""
+
+
 import sys
-import os
 import json
-"""load add and save file"""
+from os.path import exists
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-
-if os.path.exists(filename):
+if exists(filename):
     my_list = load_from_json_file(filename)
 else:
     my_list = []
 
-
 my_list.extend(sys.argv[1:])
-
 
 save_to_json_file(my_list, filename)
