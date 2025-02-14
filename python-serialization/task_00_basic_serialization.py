8d4fcf5cc879456e564ed2b411ccc44d52dc64af
Zeynab <zsakho58@gmail.com>
2025-02-14 21:37:40 +0100

    fix the code

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""0. Basic Serialization"""
-import pickle
-
+import json
 
 def serialize_and_save_to_file(data, filename):
-    """function that serialize and save data"""
-    with open(filename, 'wb') as f:
-        pickle.dump(data, f)
-
+    """
+    Sérialiser un dictionnaire Python et l'enregistrer dans un fichier JSON.
+    
+    :param data: Un dictionnaire Python contenant les données
+    :param filename: Le nom du fichier de sortie JSON. Si le fichier existe déjà, il sera remplacé.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
-    """function that load and deserialize data"""
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
+    """
+    Charger et désérialiser un fichier JSON pour recréer un dictionnaire Python.
+    
+    :param filename: Le nom du fichier JSON d'entrée
+    :return: Un dictionnaire Python contenant les données désérialisées du fichier
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
