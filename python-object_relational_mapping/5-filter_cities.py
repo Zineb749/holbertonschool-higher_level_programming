ac7e186ae2b8349ecffb697693e670b89d82f2d3
Zeynab <zsakho58@gmail.com>
2025-03-06 15:20:50 +0100

    filter cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" script that list all cities of the state in arg"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name \
+                FROM cities \
+                INNER JOIN states \
+                ON cities.state_id = states.id \
+                WHERE states.name = '{}' \
+                ORDER BY cities.id ASC".format(sys.argv[4]))
+    rows = cursor.fetchall()
+
+    print(", ".join([row[1] for row in rows]))
+
+    cursor.close()
+    db.close()
